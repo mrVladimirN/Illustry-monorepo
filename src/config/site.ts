1fb3241dff3edbd292e439fa27fbb77ebfae9a6f
mrVladimirN <vladimir.nitu.business@outlook.com>
2023-12-23 13:23:41 -0800

    feature: update siteconfig

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,3 +1,6 @@
+import { visualizationTypesEnum } from '@/lib/validation/visualizations';
+import { v4 as randomUUID } from 'uuid';
+
 export const siteConfig = {
   name: 'Illustry',
   description: 'An open source visualization hub',
@@ -14,11 +17,16 @@ export const siteConfig = {
       description: 'All visualizations',
       items: []
     },
-
     {
       title: 'Theme',
       href: '/theme',
       description: 'Set theme',
+      items: []
+    },
+    {
+      title: 'Playground',
+      href: '/playground',
+      description: 'Playground ',
       items: []
     }
   ],
@@ -85,6 +93,190 @@ export const siteConfig = {
         value: 16
       }
     ]
+  },
+  matrix: {
+    nodes: [
+      {
+        category: '1',
+        name: 'Node1',
+        labels: [
+          {
+            name: 'Label1',
+            value: 1,
+            properties: {
+              style: {
+                'font-weight': 'bold',
+                'background-color': '#541690',
+                'background-color1': '541690'
+              }
+            }
+          },
+          {
+            name: 'Label2',
+            value: 0,
+            properties: [
+              {
+                style: {
+                  'font-weight': 'bold',
+                  'background-color': '#541690',
+                  'background-color1': '541690'
+                }
+              }
+            ]
+          }
+        ]
+      },
+      {
+        category: '2',
+        name: 'Node2',
+        labels: [
+          {
+            name: 'Label3',
+            value: 1,
+            properties: {
+              style: {
+                'font-weight': 'bold',
+                'background-color': '#541690',
+                'background-color1': '541690'
+              }
+            }
+          },
+          {
+            name: 'Label4',
+            value: 0,
+            properties: [
+              {
+                style: {
+                  'font-weight': 'bold',
+                  'background-color': '#541690',
+                  'background-color1': '541690'
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ],
+    links: [
+      {
+        source: 'Node1',
+        target: 'Node2',
+        value: 1,
+        properties: {
+          style: {
+            'font-weight': 'bold',
+            'background-color': '#541690',
+            'background-color1': '541690'
+          },
+          a: 'b'
+        }
+      },
+      {
+        source: 'Node2',
+        target: 'Node1',
+        value: 1,
+        properties: {
+          style: {
+            'font-weight': 'bold',
+            'background-color': '#541690',
+            'background-color1': '541690'
+          },
+          a: 'b'
+        }
+      }
+    ]
+  },
+  timeline: {
+    '2023-10-07': {
+      summary: {
+        title: 'Sample Timeline'
+      },
+      events: [
+        {
+          summary: 'Event 1',
+          date: '08:00:00',
+          type: 'Type A',
+          author: 'Author 1',
+          tags: [{ name: 'Tag A' }],
+          description: 'Description of Event 1'
+        },
+        {
+          summary: 'Event 2',
+          date: '09:00:00',
+          type: 'Type B',
+          author: 'Author 2'
+        }
+      ]
+    },
+    '2023-10-10': {
+      summary: {
+        title: 'Sample Timeline'
+      },
+      events: [
+        {
+          summary: 'Event 3',
+          date: '09:00:00',
+          type: 'Type C',
+          author: 'Author 3'
+        },
+        {
+          summary: 'Event 4',
+          date: '10:00:00',
+          type: 'Type D',
+          author: 'Author 4'
+        },
+        {
+          summary: 'Event 5',
+          date: '10:00:00',
+          type: 'Type E',
+          author: 'Author 5'
+        }
+      ]
+    },
+    '2023-10-08': {
+      summary: {
+        title: 'Sample Timeline'
+      },
+      events: [
+        {
+          summary: 'Event 6',
+          date: '11:00:00',
+          type: 'Type F',
+          author: 'Author 6'
+        },
+        {
+          summary: 'Event 7',
+          date: '11:00:00',
+          type: 'Type G',
+          author: 'Author 7'
+        },
+        {
+          summary: 'Event 8',
+          date: '12:00:00',
+          type: 'Type H',
+          author: 'Author 8'
+        }
+      ]
+    },
+    '2023-10-06 ': {
+      summary: {
+        title: 'Sample Timeline'
+      },
+      events: [
+        {
+          summary: 'Event 9',
+          date: '12:00:00',
+          type: 'Type I',
+          author: 'Author 9'
+        },
+        {
+          summary: 'Event 10',
+          date: '13:00:00',
+          type: 'Type J',
+          author: 'Author 10'
+        }
+      ]
+    }
   },
   words: [
     {
@@ -225,7 +417,6 @@ export const siteConfig = {
       { value: [0.355083, -3.376585], category: '2' },
       { value: [1.852435, 3.547351], category: '1' },
       { value: [-2.078973, 2.552013], category: '1' }
-
     ]
   },
   hierarchy: {
@@ -285,6 +476,156 @@ export const siteConfig = {
           }
         ]
       }
+    ]
+  },
+  visualizations: [
+    {
+      name: visualizationTypesEnum.BAR_CHART,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.CALENDAR,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.FORCE_DIRECTED_GRAPH,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.FUNNEL,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.HIERARCHICAL_EDGE_BUNDLING,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.LINE_CHART,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.MATRIX,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.PIE_CHART,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.SANKEY,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.SCATTER,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.SUNBURST,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.TIMELINE,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.TREEMAP,
+      id: randomUUID()
+    },
+    {
+      name: visualizationTypesEnum.WORD_CLOUD,
+      id: randomUUID()
+    }
+  ],
+  colorPallets: {
+    FreshMeadow: [
+      '#5DBE6E',
+      '#4C8BF5',
+      '#F0AC40',
+      '#D73D6C',
+      '#1D7A8A',
+      '#B65911',
+      '#84BA5B'
+    ],
+    OceanBreeze: [
+      '#348AA7',
+      '#54968F',
+      '#8AB8A8',
+      '#EFC050',
+      '#45B29D',
+      '#F07A18',
+      '#D9544D'
+    ],
+    SunsetVibes: [
+      '#FF6B6B',
+      '#FFE66D',
+      '#6B5B95',
+      '#70C1B3',
+      '#F9A03F',
+      '#F7CAC9',
+      '#92A8D1'
+    ],
+    EnchantedForest: [
+      '#00539C',
+      '#89BD9E',
+      '#5DBE6E',
+      '#FF9933',
+      '#EFC88B',
+      '#5A7247',
+      '#360745'
+    ],
+    CityLights: [
+      '#F6D55C',
+      '#3CAEA3',
+      '#ED553B',
+      '#20639B',
+      '#173F5F',
+      '#3B5998',
+      '#F05D23'
+    ],
+    VintageHues: [
+      '#DE8A5A',
+      '#9A8B4F',
+      '#005792',
+      '#3C1053',
+      '#7A306C',
+      '#8D5B4C',
+      '#C98344'
+    ],
+    DreamyPastels: [
+      '#FFD700',
+      '#FF9A8B',
+      '#87CEFA',
+      '#D4AF37',
+      '#98FB98',
+      '#B19CD9',
+      '#FFC0CB'
+    ],
+    TropicalParadise: [
+      '#F2AA4C',
+      '#0077B6',
+      '#90BE6D',
+      '#DA627D',
+      '#5E60CE',
+      '#577590',
+      '#6A0572'
+    ],
+    MidnightMagic: [
+      '#23022E',
+      '#65187A',
+      '#8A3B6B',
+      '#2C5F2D',
+      '#1D262A',
+      '#4C2C69',
+      '#51344D'
+    ],
+    EarthyTones: [
+      '#BC8B66',
+      '#393D3F',
+      '#63707D',
+      '#48484A',
+      '#6B4226',
+      '#C4C8C5',
+      '#3A3D40'
     ]
   }
 };
