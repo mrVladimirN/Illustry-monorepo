e77efec6c1091eaa98c98d4d77c3d95d18dfca43
mrVladimirN <vladimir.nitu.business@outlook.com>
2023-12-25 10:01:38 -0800

    feature: fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mrVladimirN.github.io',
-	base: '/IllustryDocs'
+	base: '/IllustryDocs',
 	integrations: [
 		starlight({
 			title: 'Illustry',
